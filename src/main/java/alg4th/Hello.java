2b894c5ea8a63bc17a4e0bd75ea7206e66e0d0ac
tianyongwei <869358189@qq.com>
2018-03-14 14:06:48 +0800

    ignore

--- src/main/java/alg4th/Hello.java
+++ src/main/java/alg4th/Hello.java
@@ -1,4 +1,9 @@
 package main.java.alg4th;
 
 public class Hello {
+    public static void main(String[] args) {
+        for (int i = 0; i <3; i++) {
+            System.out.println(123);
+        }
+    }
 }
