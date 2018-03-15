63af5023beb96e8a7ac1183e2d9cab6a48909b70
tianyongwei <869358189@qq.com>
2018-03-15 17:35:36 +0800

    add lintcode and nowcoder

---
+++ src/main/java/nowcoder/Hello.java
@@ -0,0 +1,9 @@
+package main.java.nowcoder;
+
+public class Hello {
+    public static void main(String[] args) {
+        for (int i = 0; i <3; i++) {
+            System.out.println(123);
+        }
+    }
+}
