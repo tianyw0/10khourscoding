39d2fcb1e6da02b66762cc50487e714691d5a3fd
tianyongwei <869358189@qq.com>
2018-03-15 19:53:11 +0800

    add juky

---
+++ src/main/java/july/Hello.java
@@ -0,0 +1,9 @@
+package main.java.july;
+
+public class Hello {
+    public static void main(String[] args) {
+        for (int i = 0; i <3; i++) {
+            System.out.println(123);
+        }
+    }
+}
