12b46e5f1bb404f824a1653279e310685964f250
tianyongwei <869358189@qq.com>
2018-11-26 16:51:33 +0800

    fix

--- src/main/java/leetcode/Problem_001_two_sum.java
+++ src/main/java/leetcode/Problem_001_two_sum.java
@@ -1,12 +1,13 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Problem_001_two_sum {
 	public static int[] twoSum(int[] nums, int target) {
 		int[] result = new int[2];
-		Map<Integer, Integer> flags = new HashMap<>();
+		Map<Integer, Integer> flags = new HashMap<Integer, Integer>();
 		for (int i = 0; i < nums.length; i++) {
 			int num = nums[i];
 			if (flags.containsKey(num)) {
@@ -19,23 +20,9 @@ public class Problem_001_two_sum {
 		return result;
 	}
 	
-	public int[] twoSumFromLeetcode(int[] nums, int target) {
-	    Map<Integer, Integer> map = new HashMap<>();
-	    for (int i = 0; i < nums.length; i++) {
-	        int complement = target - nums[i];
-	        if (map.containsKey(complement)) {
-	            return new int[] { map.get(complement), i };
-	        }
-	        map.put(nums[i], i);
-	    }
-	    throw new IllegalArgumentException("No two sum solution");
-	}
-
 	public static void main(String[] args) {
 		int[] sums = { -3, 4, 3, 90 };
-		int[] ddd = twoSum(sums, 0);
-		for (int i : ddd) {
-			System.out.println(i);
-		}
+		int[] ddd = twoSum(sums, 90);
+		System.out.println(Arrays.toString(ddd));
 	}
 }
