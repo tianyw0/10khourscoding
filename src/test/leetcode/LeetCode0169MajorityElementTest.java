4bdae74e95b6fef49abf9f58f433863fa1f708bf
tianyw <869358189@qq.com>
2020-08-26 00:23:45 +0800

    1

---
+++ src/test/leetcode/LeetCode0169MajorityElementTest.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0169MajorityElementTest {
+
+    LeetCode0169MajorityElement majorityElement
+            = new LeetCode0169MajorityElement();
+
+    @Test
+    void majorityElement() {
+        assertEquals(2, majorityElement.solution(new int[]{2,2,3}));
+        assertEquals(2, majorityElement.solution(new int[]{2,2,1,1,1,2,2}));
+    }
+}
