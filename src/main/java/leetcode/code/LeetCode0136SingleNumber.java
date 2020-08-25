3a2f73724e9651404e94af40dded71a84fcacdbd
tianyw <869358189@qq.com>
2020-08-25 23:50:23 +0800

    1

---
+++ src/main/java/leetcode/code/LeetCode0136SingleNumber.java
@@ -0,0 +1,7 @@
+package leetcode.code;
+
+public class LeetCode0136SingleNumber {
+    public int searchInsert(int[] nums, int target) {
+        return 0;
+    }
+}
---
+++ src/test/leetcode/code/LeetCode0136SingleNumberTest.java
@@ -0,0 +1,16 @@
+package leetcode.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0136SingleNumberTest {
+
+    private LeetCode0136SingleNumber singleNumber
+            = new LeetCode0136SingleNumber();
+
+    @Test
+    void searchInsert() {
+
+    }
+}
