0542e35efea93e81e93a17f4b54a2242b158a882
tianyw <869358189@qq.com>
2020-08-29 23:58:17 +0800

    1

--- src/test/leetcode/LeetCode0887SuperEggDropTest.java
+++ src/test/leetcode/LeetCode0887SuperEggDropTest.java
@@ -11,6 +11,17 @@ class LeetCode0887SuperEggDropTest {
 
     @Test
     void superEggDrop() {
-        assertEquals(1, superEggDrop.superEggDrop(2, 6));
+//        assertEquals(3, superEggDrop.superEggDrop(2, 6));
+//        assertEquals(4, superEggDrop.superEggDrop(3, 14));
+//        assertEquals(1, superEggDrop.superEggDrop(2, 1));
+//        assertEquals(19, superEggDrop.superEggDrop(4, 5000));
+    }
+
+    @Test
+    void testSuperEggDrop_p() {
+//        assertEquals(3, superEggDrop.superEggDrop_p(2, 6));
+//        assertEquals(4, superEggDrop.superEggDrop_p(3, 14));
+//        assertEquals(1, superEggDrop.superEggDrop_p(2, 1));
+        assertEquals(19, superEggDrop.superEggDrop_p(4, 5000));
     }
 }
