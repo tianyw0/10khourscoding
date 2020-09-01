a94c51661faec68aa0db554f039f98c479bd691b
869358189@qq.com <869358189@qq.com>
2020-09-01 09:22:26 +0800

    1

--- src/main/java/leetcode/LeetCode0240Searcha2DMatrixII.java
+++ src/main/java/leetcode/LeetCode0240Searcha2DMatrixII.java
@@ -2,24 +2,23 @@ package leetcode;
 
 public class LeetCode0240Searcha2DMatrixII {
     public boolean searchMatrix(int[][] matrix, int target) {
-        System.out.println(matrix.length);
-        System.out.println(matrix[0].length);
         if(matrix.length == 0) {
             return false;
         }
-        int xlen = matrix[0].length;
-        int ylen = matrix.length;
+        int xlen = matrix.length;
+        int ylen = matrix[0].length;
 
-        int xloc = 0, yloc = ylen - 1;
+        // 从左下角开始找，直到超出边界
+        int xloc = xlen-1, yloc = 0;
         while(isOK(xloc, xlen) && isOK(yloc, ylen)) {
             if(matrix[xloc][yloc] == target) {
                 return true;
             } else if(matrix[xloc][yloc] > target) {
-                // 开始找matrix[xloc][yloc+1]
-                yloc++;
-            } else if(matrix[xloc][yloc] < target) {
                 // 开始找matrix[xloc-1][yloc]
                 xloc--;
+            } else if(matrix[xloc][yloc] < target) {
+                // 开始找matrix[xloc][yloc+1]
+                yloc++;
             }
         }
         return false;
--- src/test/leetcode/LeetCode0240Searcha2DMatrixIITest.java
+++ src/test/leetcode/LeetCode0240Searcha2DMatrixIITest.java
@@ -12,5 +12,7 @@ class LeetCode0240Searcha2DMatrixIITest {
     @Test
     void searchMatrix() {
         assertEquals(true, searcha2DMatrixII.searchMatrix(new int[][]{{1,4,7,11,15},{2,5,8,12,19}, {3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}},5));
+        assertEquals(false, searcha2DMatrixII.searchMatrix(new int[][]{},0));
+        assertEquals(false, searcha2DMatrixII.searchMatrix(new int[][]{{1,1}},0));
     }
 }
