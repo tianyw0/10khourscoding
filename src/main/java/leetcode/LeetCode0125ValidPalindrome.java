5b0ab5aebd91715ef235c042821a1226703d8e8a
869358189@qq.com <869358189@qq.com>
2020-09-01 09:40:33 +0800

    1

---
+++ src/main/java/leetcode/LeetCode0125ValidPalindrome.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class LeetCode0125ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int len = s.length();
+        if(len == 0) return true;
+
+        // double point
+        int lo = 0, hi = len-1;
+        while (lo <= hi) {
+            char loChar = s.charAt(lo);
+            char hiChar = s.charAt(hi);
+            if(!Character.isLetterOrDigit(loChar)) {
+                lo++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(hiChar)) {
+                hi--;
+                continue;
+            }
+
+            if(Character.toLowerCase(loChar) != Character.toLowerCase(hiChar)) {
+                return false;
+            } else {
+                lo++;
+                hi--;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/leetcode/LeetCode0125ValidPalindromeTest.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0125ValidPalindromeTest {
+
+    LeetCode0125ValidPalindrome validPalindrome
+            = new LeetCode0125ValidPalindrome();
+    @Test
+    void isPalindrome() {
+        assertEquals(true, validPalindrome.isPalindrome("A man, a plan, a canal: Panama"));
+        assertEquals(true, validPalindrome.isPalindrome(""));
+        assertEquals(true, validPalindrome.isPalindrome("1"));
+        assertEquals(true, validPalindrome.isPalindrome("121"));
+        assertEquals(false, validPalindrome.isPalindrome("1212"));
+    }
+}
