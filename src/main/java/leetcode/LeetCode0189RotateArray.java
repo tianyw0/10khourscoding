5bdd40856e7751aec80e34eb1566cadd12b6ffa5
869358189@qq.com <869358189@qq.com>
2020-09-02 13:48:12 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0189RotateArray.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class LeetCode0189RotateArray {
+    public void rotate(int[] nums, int k) {
+        if(k % nums.length == 0) {
+            return;
+        }
+        k = k % nums.length;
+        Queue<Integer> queue =  new LinkedList<>();
+        int loc = nums.length - k;
+        // 执行num.length次入队的操作
+        for (int i = 0; i < nums.length; i++) {
+            if(loc >= nums.length) {
+                loc %= nums.length;
+            }
+            queue.offer(nums[loc]);
+            loc++;
+        }
+
+        // 出队填充
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = queue.poll();
+        }
+        // System.out.println(Arrays.toString(nums));
+    }
+}
---
+++ src/test/leetcode/LeetCode0189RotateArrayTest.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0189RotateArrayTest {
+
+    LeetCode0189RotateArray rotateArray
+            = new LeetCode0189RotateArray();
+    @Test
+    void rotate() {
+        int[] ints = {1, 2, 3, 4};
+        rotateArray.rotate(ints, 3);
+        assertArrayEquals(new int[]{2,3,4,1}, ints);
+    }
+}
