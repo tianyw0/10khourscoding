6655762f5e745f6e91a26e4056604c578eb8a254
869358189@qq.com <869358189@qq.com>
2020-09-02 11:19:11 +0800

    AC

---
+++ src/test/leetcode/LeetCode0344ReverseStringTest.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0344ReverseStringTest {
+
+    LeetCode0344ReverseString reverseString
+            = new LeetCode0344ReverseString();
+
+    @Test
+    void reverseString() {
+        char[] chars = {'a', 'b', 'c'};
+        reverseString.reverseString(chars);
+        assertArrayEquals(new char[]{'c','b','a'}, chars);
+    }
+}
