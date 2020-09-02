f514ec61fb79979d6235bdb0717e13e9b36ab1ab
869358189@qq.com <869358189@qq.com>
2020-09-02 18:05:05 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0334IncreasingTripletSubsequence.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class LeetCode0334IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums.length < 3) {
+            return false;
+        }
+
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        //
+    }
+}
---
+++ src/test/leetcode/LeetCode0334IncreasingTripletSubsequenceTest.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0334IncreasingTripletSubsequenceTest {
+
+    LeetCode0334IncreasingTripletSubsequence increasingTripletSubsequence
+            = new LeetCode0334IncreasingTripletSubsequence();
+
+    @Test
+    void increasingTriplet1() {
+        assertEquals(true, increasingTripletSubsequence.increasingTriplet(new int[]{1,2,3,4}));
+    }
+    @Test
+    void increasingTriplet2() {
+        assertEquals(false, increasingTripletSubsequence.increasingTriplet(new int[]{1,4,3,4}));
+    }
+    @Test
+    void increasingTriplet3() {
+        assertEquals(true, increasingTripletSubsequence.increasingTriplet(new int[]{5,1,5,5,2,5,4}));
+    }
+
+    @Test
+    void increasingTriplet4() {
+        assertEquals(true, increasingTripletSubsequence.increasingTriplet(new int[]{1,0,0,0,10,0,0,0,0,100}));
+    }
+}
