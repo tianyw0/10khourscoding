3dedceaed5df3fc3c490b6783e1b927f4bda08ed
tianyongwei <869358189@qq.com>
2020-09-05 10:45:28 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0144BinaryTreePreorderTraversal.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+import java.util.List;
+
+public class LeetCode0144BinaryTreePreorderTraversal {
+  public List<Integer> preorderTraversal(TreeNode root) {
+    return null;
+  }
+}
