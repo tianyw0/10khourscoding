0216e27818dc0802c7bad0329138fe8357dd4d25
tianyw <869358189@qq.com>
2020-09-06 10:49:51 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0101SymmetricTree2.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LeetCode0101SymmetricTree2 {
+    public boolean isSymmetric(TreeNode root) {
+        // 使用队列模拟递归实现
+        // 初始化队列
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if(left == null && right == null) continue;
+
+            // 根节点不等
+            if(left == null) {
+                return false;
+            }
+            if(right == null) {
+                return false;
+            }
+            if(left.val != right.val) {
+                return false;
+            }
+
+            queue.add(left.left);
+            queue.add(right.right);
+
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    }
+}
