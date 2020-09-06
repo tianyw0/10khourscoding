ce4ffe04da3e198827ddb5bef07bfe80589de483
tianyw <869358189@qq.com>
2020-09-06 09:10:33 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0104MaximumDepthofBinaryTree.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+public class LeetCode0104MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int ans = 1;
+        ans += Math.max(maxDepth(root.left), maxDepth(root.right));
+        return ans;
+    }
+}
