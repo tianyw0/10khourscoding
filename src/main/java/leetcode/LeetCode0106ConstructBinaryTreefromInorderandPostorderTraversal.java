c091d212993f12406cf65cbff0331571554b017a
tianyw <869358189@qq.com>
2020-09-08 07:30:55 +0800

    AC

--- src/main/java/leetcode/LeetCode0106ConstructBinaryTreefromInorderandPostorderTraversal.java
+++ src/main/java/leetcode/LeetCode0106ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -3,43 +3,68 @@ package leetcode;
 import leetcode.common.TreeNode;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
 
 public class LeetCode0106ConstructBinaryTreefromInorderandPostorderTraversal {
 
-    /**
-     * TreeNode node = new TreeNode(1
-     *                 , new TreeNode(2, new TreeNode(4), new TreeNode(5))
-     *                 , new TreeNode(3, new TreeNode(6), new TreeNode(7)));
-     *         assertEquals(node, traversal.buildTree(new int[]{4,2,5,1,6,3,7}, new int[]{4,5,2,6,7,3,1}));
-     * @param inorder
-     * @param postorder
-     * @return
-     */
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>(){{
+        if(inorder == null || inorder.length == 0) {
+            return null;
+        }
+        // 重点是理解：根是处理一棵树的最小单位，因为根是一棵树的最小必要元素
+        Map<Integer, Integer> inMap = new HashMap<Integer, Integer>(){{
             for (int i = 0; i < inorder.length; i++) {
                 put(inorder[i], i);
             }
         }};
-
-
-        int root = postorder[postorder.length - 1];
-        TreeNode rootNode = new TreeNode(root);
-        // 初始化队列
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(root);
-        ;
-        return rootNode;
+        Map<Integer, Integer> postMap = new HashMap<Integer, Integer>(){{
+            for (int i = 0; i < postorder.length; i++) {
+                put(postorder[i], i);
+            }
+        }};
+        return build(inMap, postMap, inorder, 0, inorder.length-1, postorder, 0, postorder.length-1);
     }
 
-    private void build(TreeNode rootNode, int root, Map<Integer, Integer> map, int lo, int hi) {
-        // 构建左子树
-        // 构建右子树
-//        build(lo, map.get(root))
+    private TreeNode build(Map<Integer, Integer> inMap, Map<Integer, Integer> postMap, int[] inorder, int inLo, int inHi, int[] postorder, int postLo, int postHi) {
+        int rootVal = postorder[postHi];
+        int rootInx = inMap.get(rootVal);
+        TreeNode root = new TreeNode(rootVal);
 
+        // 只有一个元素的时候直接返回
+        if(inLo == inHi) {
+            return root;
+        }
+
+        if(inLo < rootInx) {
+            // 有左子树
+            // 确定边界
+            int inLeft = inLo;
+            int inRight = rootInx-1;
+            int postLeft = postLo;
+//            int postRight = postMap.get(inorder[rootInx+1>inHi ? rootInx : rootInx+1]) - 1;
+            int postRight = getPostRihgt(inorder, postorder, inMap, postMap, rootInx, inLo, inHi);
+            root.left = build(inMap, postMap, inorder, inLeft, inRight, postorder, postLeft, postRight);
+        }
+
+        if(rootInx < inHi) {
+            // 有右子树
+            // 确定边界
+            int inLeft = rootInx+1;
+            int inRight = inHi;
+            int postLeft = postMap.get(inorder[rootInx+1]);
+            int postRight = postHi-1;
+            root.right = build(inMap, postMap, inorder, inLeft, inRight, postorder, postLeft, postRight);
+        }
+        return root;
+    }
+
+    private int getPostRihgt(int[] inorder, int[] postorder, Map<Integer, Integer> inMap, Map<Integer, Integer> postMap, int rootInx, int inLo, int inHi) {
+        if(rootInx + 1 > inHi) return rootInx-1;
+
+        int min = Integer.MAX_VALUE;
+        for (int i = rootInx+1; i <= inHi; i++) {
+            min = Math.min(postMap.get(inorder[i]), min);
+        }
+        return min-1;
     }
 }
--- src/test/leetcode/LeetCode0106ConstructBinaryTreefromInorderandPostorderTraversalTest.java
+++ src/test/leetcode/LeetCode0106ConstructBinaryTreefromInorderandPostorderTraversalTest.java
@@ -19,10 +19,50 @@ class LeetCode0106ConstructBinaryTreefromInorderandPostorderTraversalTest {
     }
 
     @Test
+    void buildTree1() {
+        TreeNode node = new TreeNode(3);
+        assertEquals(node, traversal.buildTree(new int[]{3}, new int[]{3}));
+    }
+
+    @Test
     void buildTree2() {
         TreeNode node = new TreeNode(1
                 , new TreeNode(2, new TreeNode(4), new TreeNode(5))
                 , new TreeNode(3, new TreeNode(6), new TreeNode(7)));
         assertEquals(node, traversal.buildTree(new int[]{4,2,5,1,6,3,7}, new int[]{4,5,2,6,7,3,1}));
+    }
+
+    @Test
+    void buildTree5() {
+        TreeNode node = new TreeNode(2
+                , new TreeNode(1)
+                , new TreeNode(3, null, new TreeNode(4)));
+        assertEquals(node, traversal.buildTree(new int[]{1,2,3,4}, new int[]{1,4,3,2}));
+    }
+
+    /**
+     *输入：
+     * [1,3,2]
+     * [3,2,1]
+     * 输出：
+     * [1,null,2,2]
+     * 预期结果：
+     * [1,null,2,3]
+     */
+    @Test
+    void buildTree6() {
+        TreeNode node = new TreeNode(1
+                , new TreeNode(2), null);
+        assertEquals(node, traversal.buildTree(new int[]{2,1}, new int[]{2,1}));
+    }
+
+    @Test
+    void buildTree3() {
+        assertEquals(null, traversal.buildTree(new int[]{}, new int[]{}));
+    }
+
+    @Test
+    void buildTree4() {
+        assertEquals(null, traversal.buildTree(null, null));
     }
 }
