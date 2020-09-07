d6992ed42c2fb91bc9a6efb3a9bf6fbba3f7b33a
tianyw <869358189@qq.com>
2020-09-08 04:52:41 +0800

    AC

--- src/main/java/leetcode/LeetCode0297SerializeandDeserializeBinaryTree.java
+++ src/main/java/leetcode/LeetCode0297SerializeandDeserializeBinaryTree.java
@@ -2,7 +2,9 @@ package leetcode;
 
 import leetcode.common.TreeNode;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class LeetCode0297SerializeandDeserializeBinaryTree {
@@ -11,6 +13,7 @@ public class LeetCode0297SerializeandDeserializeBinaryTree {
         if(root == null) return "[]";
 
         StringBuilder sb = new StringBuilder("[");
+        StringBuilder nullsb = new StringBuilder("");
 
         // 按层遍历
         Queue<TreeNode> queue = new LinkedList<>();
@@ -21,8 +24,10 @@ public class LeetCode0297SerializeandDeserializeBinaryTree {
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 if(node == null) {
-                    sb.append("null,");
+                    nullsb.append("null,");
                 } else {
+                    sb.append(nullsb);
+                    nullsb.delete(0, nullsb.length());
                     sb.append(node.val + ",");
                     queue.add(node.left);
                     queue.add(node.right);
@@ -36,6 +41,47 @@ public class LeetCode0297SerializeandDeserializeBinaryTree {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        return null;
+
+        if("[]".equalsIgnoreCase(data)) return null;
+
+        // "[1,2,3,null,null,4,5]"
+        String[] eles = data.substring(1, data.length()-1).split(",");
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        Queue<String> strQueue = new LinkedList<>(Arrays.asList(eles));
+        TreeNode root = new TreeNode(Integer.valueOf(eles[0]));
+        nodeQueue.add(root);
+        strQueue.poll();
+        while (!strQueue.isEmpty() && !nodeQueue.isEmpty()) {
+            int nodeQueueSize = nodeQueue.size();
+            for (int i = 0; i < nodeQueueSize; i++) {
+                TreeNode node = nodeQueue.poll();
+                // left
+                String strleft = strQueue.poll();
+                if(strleft == null) {
+                    node.left = null;
+                } else {
+                    if("null".equalsIgnoreCase(strleft)) {
+                        node.left = null;
+                    } else {
+                        node.left = new TreeNode(Integer.valueOf(strleft));
+                        nodeQueue.add(node.left);
+                    }
+                }
+
+                // right
+                String strright = strQueue.poll();
+                if(strright == null) {
+                    node.right = null;
+                } else {
+                    if("null".equalsIgnoreCase(strright)) {
+                        node.right = null;
+                    } else {
+                        node.right = new TreeNode(Integer.valueOf(strright));
+                        nodeQueue.add(node.right);
+                    }
+                }
+            }
+        }
+        return root;
     }
 }
--- src/test/leetcode/LeetCode0297SerializeandDeserializeBinaryTreeTest.java
+++ src/test/leetcode/LeetCode0297SerializeandDeserializeBinaryTreeTest.java
@@ -25,6 +25,15 @@ class LeetCode0297SerializeandDeserializeBinaryTreeTest {
     }
 
     @Test
+    void serialize3() {
+        TreeNode node = new TreeNode(1
+                , new TreeNode(2, new TreeNode(6), null)
+                , new TreeNode(3, new TreeNode(4), new TreeNode(5, new TreeNode(6), null)));
+
+        assertEquals("[1,2,3,6,null,4,5,null,null,null,null,6]", l297.serialize(node));
+    }
+
+    @Test
     void deserialize2() {
         assertEquals(null, l297.deserialize("[]"));
     }
@@ -35,6 +44,13 @@ class LeetCode0297SerializeandDeserializeBinaryTreeTest {
                 , new TreeNode(2)
                 , new TreeNode(3, new TreeNode(4), new TreeNode(5)));
 
-        assertEquals(node, l297.deserialize("[1,2,3,null,null,4,5]"));
+        assertEquals(node.toString(), l297.deserialize("[1,2,3,null,null,4,5]").toString());
+    }
+
+    @Test
+    void deserialize3() {
+        TreeNode node = new TreeNode(1);
+
+        assertEquals(node.toString(), l297.deserialize("[1]").toString());
     }
 }
