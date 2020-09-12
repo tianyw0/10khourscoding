74065a41b9ee3f3a0153049ac3ac181f63d12148
tianyw <869358189@qq.com>
2020-09-12 18:23:33 +0800

    AC

--- src/main/java/leetcode/LeetCode0707DesignLinkedList.java
+++ src/main/java/leetcode/LeetCode0707DesignLinkedList.java
@@ -4,50 +4,74 @@ import leetcode.common.ListNode;
 
 public class LeetCode0707DesignLinkedList {
     /** Initialize your data structure here. */
-    // 看看暴露的接口，使用双节点实现和单节点实现都是可以的，跟节点元素无关。
-    // 先设计单向链表方式
-    // 初始化构造方法
     int size;
     ListNode head;
 
-    // todo 待实现
+    // 构造一个带哨兵节点的单向链表
     public LeetCode0707DesignLinkedList() {
         head = new ListNode(0);
-        size = 1;
+        size = 0;
     }
 
-    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-    // 获取第几个元素
-    public int get(int index) {
-        if(index >= size || index < 0) return -1;
-
-        for (int i = 0; i < 10; i++) {
-            
-        }
-        return 0;
-    }
-
-    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    /** Add a node of value val before the first element of the linked list.
+     * After the insertion, the new node will be the first node of the linked list. */
     // 将元素插入头节点
     public void addAtHead(int val) {
-
+        addAtIndex(0, val);
     }
 
     /** Append a node of value val to the last element of the linked list. */
     // 将元素插入尾节点
     public void addAtTail(int val) {
-
+        addAtIndex(size, val);
     }
 
-    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    /** Add a node of value val before the index-th node in the linked list.
+     * If index equals to the length of linked list,
+     * the node will be appended to the end of linked list.
+     * If index is greater than the length, the node will not be inserted. */
     // 向第几个元素插值
     public void addAtIndex(int index, int val) {
-
+        if(0 <= index && index <= size) {
+            ListNode prev = head;
+            for (int i = 0; i < index; i++) {
+                prev = prev.next;
+            }
+            ListNode toadd = new ListNode(val);
+            if(prev.next == null) {
+                prev.next = toadd;
+            } else {
+                toadd.next = prev.next;
+                prev.next = toadd;
+            }
+            size++;
+        }
     }
 
     /** Delete the index-th node in the linked list, if the index is valid. */
-    // 删除第几个元素
     public void deleteAtIndex(int index) {
+        if(0 <= index && index < size) {
+            ListNode prev = head;
+            for (int i = 0; i < index ; i++) {
+                prev = prev.next;
+            }
 
+            prev.next = prev.next.next;
+            size--;
+        }
+    }
+
+    /** Get the value of the index-th node in the linked list.
+     * If the index is invalid, return -1. */
+    public int get(int index) {
+        if(0 <= index && index < size) {
+            ListNode prev = head;
+            for (int i = 0; i < index; i++) {
+                prev = prev.next;
+            }
+            return prev.next.val;
+        } else {
+            return -1;
+        }
     }
 }
