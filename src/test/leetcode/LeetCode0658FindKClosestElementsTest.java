f1ab4a16fc6e7572b4a4bf28a7f6beee9dd780ff
tianyongwei <869358189@qq.com>
2020-10-04 13:37:24 +0800

    AC

--- src/test/leetcode/LeetCode0658FindKClosestElementsTest.java
+++ src/test/leetcode/LeetCode0658FindKClosestElementsTest.java
@@ -27,4 +27,41 @@ class LeetCode0658FindKClosestElementsTest {
     void findClosestElements3() {
         assertEquals(new ArrayList<Integer>(){{add(2);add(3);add(4);}}, new LeetCode0658FindKClosestElements().findClosestElements(new int[]{1,2,3,4,5}, 3, 3));
     }
+    //[0,1,1,1,2,3,6,7,8,9]
+    // 9
+    // 4
+    @Test
+    void findClosestElements4() {
+        assertEquals(new ArrayList<Integer>() {{
+            add(0);
+            add(1);
+            add(1);
+            add(1);
+            add(2);
+            add(3);
+            add(6);
+            add(7);
+            add(8);
+        }}, new LeetCode0658FindKClosestElements().findClosestElements(new int[]{0, 1, 1, 1, 2, 3, 6, 7, 8, 9}, 9, 4));
+    }
+    //[0,1,2,2,2,3,6,8,8,9]
+    // 5
+    // 9
+    @Test
+    void findClosestElements5() {
+        assertEquals(new ArrayList<Integer>(){{
+            add(3);
+            add(6);
+            add(8);
+            add(8);
+            add(9);
+        }}, new LeetCode0658FindKClosestElements().findClosestElements(new int[]{0,1,2,2,2,3,6,8,8,9}, 5, 9));
+    }
+
+    @Test
+    void findClosestElements6() {
+        assertEquals(new ArrayList<Integer>(){{
+            add(1);
+        }}, new LeetCode0658FindKClosestElements().findClosestElements(new int[]{1,3}, 1, 2));
+    }
 }
