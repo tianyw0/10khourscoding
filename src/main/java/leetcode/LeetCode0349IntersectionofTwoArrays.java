3c93eec54b33715d203e2b84db155dbaea7e1346
tianyw <869358189@qq.com>
2020-10-04 22:17:00 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0349IntersectionofTwoArrays.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode0349IntersectionofTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<Integer>();
+        Set<Integer> set2 = new HashSet<Integer>();
+        for (Integer n1 : nums1) set1.add(n1);
+
+        for (Integer n2 : nums2) {
+            if(set1.contains(n2)) {
+                set2.add(n2);
+            }
+        }
+
+        int[] res = new int[set2.size()];
+        int idx = 0;
+        for (Integer s2 : set2) {
+            res[idx++] = s2;
+        }
+        return res;
+    }
+}
