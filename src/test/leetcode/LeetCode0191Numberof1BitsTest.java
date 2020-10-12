18a0438544c986919520f820950ac6edfb9fb027
869358189@qq.com <869358189@qq.com>
2020-10-12 13:09:32 +0800

    AC

---
+++ src/test/leetcode/LeetCode0191Numberof1BitsTest.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0191Numberof1BitsTest {
+
+    @Test
+    void hammingWeight() {
+        assertEquals(1, new LeetCode0191Numberof1Bits().hammingWeight(1));
+    }
+
+    @Test
+    void hammingWeight2() {
+        assertEquals(0, new LeetCode0191Numberof1Bits().hammingWeight(0));
+    }
+
+    @Test
+    void hammingWeight3() {
+        assertEquals(2, new LeetCode0191Numberof1Bits().hammingWeight(3));
+    }
+
+    @Test
+    void hammingWeight4() {
+        assertEquals(3, new LeetCode0191Numberof1Bits().hammingWeight(7));
+    }
+
+    @Test
+    void hammingWeight5() {
+        assertEquals(3, new LeetCode0191Numberof1Bits().hammingWeight(11));
+    }
+}
