7debdd595e4f1230c015c03832f7a5e7d81339ba
tianyw <869358189@qq.com>
2020-10-12 21:09:31 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0118PascalsTriangle.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode0118PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        if(numRows == 0) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        if(numRows == 1) {
+            res.add(new ArrayList<Integer>(){{add(1);}});
+            return res;
+        }
+
+        List<Integer> curr = new ArrayList<Integer>(){{add(1);}};
+        res.add(new ArrayList<Integer>(){{add(1);}});
+        for (int i = 2; i <= numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                if(j == 0 || j == i - 1) {
+                    list.add(1);
+                } else {
+                    list.add(curr.get(j)+curr.get(j-1));
+                }
+            }
+            res.add(list);
+            curr = list;
+        }
+        return res;
+    }
+}
