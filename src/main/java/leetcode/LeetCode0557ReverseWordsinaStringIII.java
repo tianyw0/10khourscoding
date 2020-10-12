cf8857ae91f20fd4d5e9edc3776a14e948e5df3c
869358189@qq.com <869358189@qq.com>
2020-10-12 10:58:52 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0557ReverseWordsinaStringIII.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+
+public class LeetCode0557ReverseWordsinaStringIII {
+    public String reverseWords(String s) {
+        StringBuilder res = new StringBuilder();
+        StringBuilder word = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == ' ') {
+                res.append(getReverseWord(word));
+                word = new StringBuilder();
+                res.append(c);
+            } else {
+                word.append(c);
+            }
+        }
+        res.append(getReverseWord(word));
+        return res.toString();
+    }
+
+    private String getReverseWord(StringBuilder word) {
+        String wordStr = word.toString();
+        StringBuilder sb = new StringBuilder();
+        for (int i = wordStr.length() - 1; i >= 0; i--) {
+            sb.append(wordStr.charAt(i));
+        }
+        return sb.toString();
+    }
+}
