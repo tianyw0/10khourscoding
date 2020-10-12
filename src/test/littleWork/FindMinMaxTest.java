9348a68fb248e3d19ed2e8e4c7e59593cad9efc9
tianyw <869358189@qq.com>
2020-10-12 20:05:01 +0800

    AC

---
+++ src/test/littleWork/FindMinMaxTest.java
@@ -0,0 +1,43 @@
+package littleWork;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindMinMaxTest {
+
+    @Test
+    void getMinMax() {
+        assertEquals(new MinMax(1,6), new FindMinMax().getMinMax(new int[]{1,2,3,4,5,6}));
+    }
+
+    @Test
+    void getMinMax2() {
+        assertEquals(new MinMax(1,2), new FindMinMax().getMinMax(new int[]{1,2}));
+    }
+
+    @Test
+    void getMinMax3() {
+        assertEquals(new MinMax(1,6), new FindMinMax().getMinMax(new int[]{1,6,6}));
+    }
+
+    @Test
+    void getMinMax4() {
+        assertEquals(new MinMax(1,1), new FindMinMax().getMinMax(new int[]{1,1,1,1,1,1}));
+    }
+
+    @Test
+    void getMinMax5() {
+        assertEquals(new MinMax(-1,6), new FindMinMax().getMinMax(new int[]{-1,2,3,4,5,6}));
+    }
+
+    @Test
+    void getMinMax6() {
+        assertEquals(new MinMax(-1,-1), new FindMinMax().getMinMax(new int[]{-1,-1}));
+    }
+
+    @Test
+    void getMinMax7() {
+        assertEquals(new MinMax(-1,-1), new FindMinMax().getMinMax(new int[]{-1}));
+    }
+}
