c5f9b623669c4a1591123117d78d0ddb2c9228f4
869358189@qq.com <869358189@qq.com>
2020-10-15 11:36:32 +0800

    AC

--- src/main/java/leetcode/LeetCode0190ReverseBits.java
+++ src/main/java/leetcode/LeetCode0190ReverseBits.java
@@ -2,9 +2,10 @@ package leetcode;
 
 public class LeetCode0190ReverseBits {
     public int reverseBits(int n) {
-        String s = Integer.toString(n, 2);
-        s = new StringBuilder(s).reverse().toString();
-        Integer integer = Integer.valueOf(s, 10);
-        return integer;
+        String unsigned = Integer.toUnsignedString(n, 2);
+        String zeros = "0".repeat(32-unsigned.length());
+        unsigned = zeros + unsigned;
+        unsigned = new StringBuilder(unsigned).reverse().toString();
+        return (int) Long.parseLong(unsigned, 2);
     }
 }
--- src/test/leetcode/LeetCode0190ReverseBitsTest.java
+++ src/test/leetcode/LeetCode0190ReverseBitsTest.java
@@ -6,13 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LeetCode0190ReverseBitsTest {
 
+    // 测试通不过，但是提交正确，Java没有无符号整型
     @Test
     void reverseBits() {
-        assertEquals(10, new LeetCode0190ReverseBits().reverseBits(2));
+        assertEquals((int) Math.pow(2, 31), new LeetCode0190ReverseBits().reverseBits(2));
     }
 
     @Test
     void reverseBits2() {
-        assertEquals("", new LeetCode0190ReverseBits().reverseBits(1));
+        assertEquals((int) Math.pow(2, 32), new LeetCode0190ReverseBits().reverseBits(1));
     }
 }
