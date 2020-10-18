827eb48542d59029d383484e3772bfb710de2bb9
tianyw <869358189@qq.com>
2020-10-18 14:00:02 +0800

    AC

--- src/main/java/leetcode/LeetCode0155MinStack.java
+++ src/main/java/leetcode/LeetCode0155MinStack.java
@@ -6,19 +6,15 @@ public class LeetCode0155MinStack {
 
     Stack<Integer> stack;
     Stack<Integer> minStack;
-    int min;
 
     public LeetCode0155MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
-        min = Integer.MAX_VALUE;
+        minStack.add(Integer.MAX_VALUE);
     }
     
     public void push(int x) {
-        if(x < min) {
-            min = x;
-        }
-        minStack.add(min);
+        minStack.add(Math.min(minStack.peek(), x));
         stack.add(x);
     }
     
