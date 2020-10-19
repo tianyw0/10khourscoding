fa244095d2f8c79b4c29d2cc4aa716d77c20c3c3
tianyw <869358189@qq.com>
2020-10-19 21:53:48 +0800

    AC

---
+++ src/test/leetcode/LeetCode0242ValidAnagramTest.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0242ValidAnagramTest {
+
+    @Test
+    void isAnagram() {
+        assertEquals(false, new LeetCode0242ValidAnagram()
+                .isAnagram("cat", "car"));
+    }
+
+    @Test
+    void isAnagram2() {
+        assertEquals(true, new LeetCode0242ValidAnagram()
+                .isAnagram("cat", "tac"));
+    }
+
+    @Test
+    void isAnagram3() {
+        assertEquals(true, new LeetCode0242ValidAnagram()
+                .isAnagram("", ""));
+    }
+
+}
