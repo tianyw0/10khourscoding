b70e06247a9bee64890a5d984af36c9fcc1ca222
869358189@qq.com <869358189@qq.com>
2020-10-19 15:45:03 +0800

    AC

---
+++ src/main/java/leetcode/LeetCode0204CountPrimes.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class LeetCode0204CountPrimes {
+    public int countPrimes(int n) {
+        // 排除法（1. 所有偶数都不要 2. 所有质数的奇数倍
+        int count = 0;
+//        for (int i = 0; i < n; i++) {
+//            if(judge(i)) {
+//                count++;
+//            }
+//        }
+        return count;
+    }
+}
+
