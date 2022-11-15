588859f72f566be6faba0e3631a44e31a54f5a5d
yongweitian <yongweitian@tencent.com>
2022-11-15 14:23:23 +0800

    rename

--- src/test/java/com/tianyongwei/algorithm/leetcode/LeetCode0053MaximumSubarrayTest.java
+++ src/test/java/com/tianyongwei/algorithm/leetcode/LeetCode0053MaximumSubarrayTest.java
@@ -8,16 +8,16 @@ class LeetCode0053MaximumSubarrayTest {
 
     @Test
     void maxSubArray() {
-        assertEquals(12, new LeetCode0053MaximumSubarray().maxSubArray(new int[]{1,2,4,5}));
+        assertEquals(12, new LeetCode0053MaximumSubArray().maxSubArray(new int[]{1,2,4,5}));
     }
 
     @Test
     void maxSubArray2() {
-        assertEquals(6, new LeetCode0053MaximumSubarray().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        assertEquals(6, new LeetCode0053MaximumSubArray().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
     }
 
     @Test
     void maxSubArray3() {
-        assertEquals(100, new LeetCode0053MaximumSubarray().maxSubArray(new int[]{-2,100}));
+        assertEquals(100, new LeetCode0053MaximumSubArray().maxSubArray(new int[]{-2,100}));
     }
 }
