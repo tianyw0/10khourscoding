7fd7a47da683f9a37ebf9d12dab7b028bde15d4f
yongweitian <yongweitian@tencent.com>
2024-03-10 21:11:35 +0800

    init a golang dir

---
+++ go/hello_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMyFunction(t *testing.T) {
+	// Write your test logic here
+}
+
+func TestAnotherFunction(t *testing.T) {
+	fmt.Println("dddd")
+	// Write your test logic here
+}
